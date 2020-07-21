54895e7eee14dcbbc37f24351a24cdf7e7655d00
Jayqq <498068957@qq.com>
2020-07-22 01:15:28 +0800

    创建timer_manager

--- timer/wheel_timer.h
+++ timer/wheel_timer.h
@@ -9,8 +9,8 @@
 //定义两级大小
 const uint16_t TVN_BITS = 6;
 const uint16_t TVR_BITS = 8;
-const uint32_t TVN_SIZE = 1 << TVN_BITS;
-const uint32_t TVR_SIZE = 1 << TVR_BITS;
+const uint32_t TVN_SIZE = 1 << TVN_BITS; //二三四五级时间轮的大小
+const uint32_t TVR_SIZE = 1 << TVR_BITS; //一级时间轮的大小
 const uint32_t TVN_MASK = TVN_SIZE - 1;
 const uint32_t TVR_MASK = TVR_SIZE - 1;
 typedef void (*timer_cb_t)(void *);
@@ -23,8 +23,9 @@ typedef struct LIST_TIMER
 
 typedef struct WHEEL_TIMER_NODE
 {
-    LISTTIMER listTImer; //双向链表
+    LISTTIMER listTImer; //双向链表的头部
     uint32_t timeout_tv; //定时器过期的时间
+    uint32_t period_tv;  //触发几次
     void *data;          //参数值
     timer_cb_t callback; //回调函数
 
@@ -33,21 +34,29 @@ typedef struct WHEEL_TIMER_NODE
 class WHEEL_TIMER_MANAGER
 {
 public:
-    void init();
-    void add_timer_node(void *data, timer_cb_t cb, uint32_t later_seconds, uint32_t later_milliseconds);
+    void InitListTimer(LISTTIMER *listtimer, uint32_t size);   //初始化一级时间轮
+    void DeleteListTimer(LISTTIMER *listtimer, uint32_t size); //删除一级时间轮
+    void ListTimerReplace(LISTTIMER *pOld, LISTTIMER *pNew);   //替换pOld
+    void AddTimerNode(TIMERNODE timer_node);
+    void CreateTimer(timer_cb_t callback, void *param, uint32_t DuringTime, uint32_t PeridTime);
     void delete_timer_node(WHEEL_TIMER_NODE *timer_node);
     void show_timer_info();
     void deal_timeout();
+    uint32_t GetBaseTimerOld();
+    uint32_t GetBaseTime(); //获取基准时间
+    void ListTimerInsert(LISTTIMER *pNew, LISTTIMER *pPrev, LISTTIMER *pNext);
+    void ListTimerInsertHead(LISTTIMER *pNew, LISTTIMER *pHead);
+    void ListTimerInsertTail(LISTTIMER *pNew, LISTTIMER *pTail);
     WHEEL_TIMER_MANAGER(/* args */);
-    ~WHEEL_TIMER_MANAGER();//析构函数这边需要删除整个时间管理器
+    ~WHEEL_TIMER_MANAGER(); //析构函数这边需要删除整个时间管理器
 
 private:
     //仿照Linux实现五个级别的时间轮，表示更大的时间
     LISTTIMER listTimer1[TVR_SIZE]; //一级时间轮，2^8=256毫秒
     LISTTIMER listTimer2[TVN_SIZE]; //二级时间轮，256～256*64-1毫秒
-    LISTTIMER listtimer3[TVN_SIZE]; //三级时间轮，256*64 ~ 256*64*64-1
-    LISTTIMER listtimer4[TVN_SIZE]; //四级时间轮，256*64*64 ~ 256*64*64*64-1
-    LISTTIMER listtimer5[TVN_SIZE]; //五级时间轮，256*64*64*64 ~ 256*64*64*64*64-1
+    LISTTIMER listTimer3[TVN_SIZE]; //三级时间轮，256*64 ~ 256*64*64-1
+    LISTTIMER listTimer4[TVN_SIZE]; //四级时间轮，256*64*64 ~ 256*64*64*64-1
+    LISTTIMER listTimer5[TVN_SIZE]; //五级时间轮，256*64*64*64 ~ 256*64*64*64*64-1
 
     uint32_t base_time; //基准时间
 };
